d7d8cab94a1b07d83c17e53b7c4220afb73bffdc
a-sasaoka <chikezo@gmail.com>
2023-06-28 16:30:42 +0900

    init

---
+++ lib/next_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class NextPage extends StatelessWidget {
+  final String title;
+  const NextPage(this.title, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Row(
+            children: [
+              SizedBox(width: 30, child: Text('ID')),
+              Text(':　毎回同じIDを表示')
+            ],
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            children: [
+              SizedBox(width: 30, child: Text('PW')),
+              Text(':　毎回同じパスワードを表示')
+            ],
+          ),
+        ]),
+      ),
+    );
+  }
+}
