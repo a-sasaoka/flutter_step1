c1a1ac90b01ee13fb66a23a89a3efa354582397b
a-sasaoka <chikezo@gmail.com>
2023-06-29 10:45:20 +0900

    Enhance

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutterラボ'),
     );
   }
 }
@@ -38,7 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Flutterラボ'),
+        title: Text(widget.title),
       ),
       body: ListView.builder(
           itemCount: titleList.length,
